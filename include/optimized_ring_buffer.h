8745c71da6f5bc09127774ff17a5ad6f98f28f50
Logan Rose <logprose@umich.edu>
2026-05-30 11:14:43 -0400

    feat: utilizing cached_* for performance speed up

--- include/optimized_ring_buffer.h
+++ include/optimized_ring_buffer.h
@@ -24,10 +24,13 @@ namespace SPSC {
 
         back = std::atomic_load_explicit(&back_, std::memory_order_relaxed);
         front = std::atomic_load_explicit(&front_, std::memory_order_acquire); 
+        front = cached_front_;
+        if ((back + 1) % N == front) {
+            cached_front_ = front = std::atomic_load_explicit(&front_, std::memory_order_acquire);
+        }
+        
         if ((back + 1) % N == front) {
             return false;
-        } else {
-
         }
 
         data_[back] = elem;
@@ -41,11 +44,15 @@ namespace SPSC {
         std::size_t back, front;
         front = std::atomic_load_explicit(&front_, std::memory_order_relaxed);
         back = std::atomic_load_explicit(&back_, std::memory_order_acquire);
+        back = cached_back_;
+        if (back == front) {
+           cached_back_ = back = std::atomic_load_explicit(&back_, std::memory_order_acquire); 
+        }
+
         if (back == front) {
             return std::nullopt;
-        } else {
-
         }
+
         T elem = data_[front];
         std::atomic_store_explicit(&front_, (front + 1) % N, std::memory_order_release);
         return elem;
