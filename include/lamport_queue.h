69d578c52edb2d015690a00bbb308935599e0e24
Logan Rose <logprose@umich.edu>
2026-05-30 10:20:52 -0400

    feat: added memory_order_(acquire|release) optimization, still passing tsan

--- include/lamport_queue.h
+++ include/lamport_queue.h
@@ -20,7 +20,7 @@ namespace SPSC {
         std::size_t back, front;
 
         back = std::atomic_load_explicit(&back_, std::memory_order_relaxed); // we own the write so can relax the read
-        front = std::atomic_load_explicit(&front_, std::memory_order_seq_cst); 
+        front = std::atomic_load_explicit(&front_, std::memory_order_acquire); 
         if ((back + 1) % N == front) {
             return false;
         } else {
@@ -29,7 +29,7 @@ namespace SPSC {
 
         data_[back] = elem;
         // give back_, store the size remaining, enforce memory sequencing
-        std::atomic_store_explicit(&back_, (back + 1) % N, std::memory_order_seq_cst); // do I need this? yes... but why?
+        std::atomic_store_explicit(&back_, (back + 1) % N, std::memory_order_release);
         return true;
     }
 
@@ -37,14 +37,14 @@ namespace SPSC {
     std::optional<T> LamportQueue<T, N>::pop() {
         std::size_t back, front;
         front = std::atomic_load_explicit(&front_, std::memory_order_relaxed); // we own the write so can relax the read
-        back = std::atomic_load_explicit(&back_, std::memory_order_seq_cst);
+        back = std::atomic_load_explicit(&back_, std::memory_order_acquire);
         if (back == front) {
             return std::nullopt;
         } else {
 
         }
         T elem = data_[front];
-        std::atomic_store_explicit(&front_, (front + 1) % N, std::memory_order_seq_cst);
+        std::atomic_store_explicit(&front_, (front + 1) % N, std::memory_order_release);
         return elem;
     }
 
