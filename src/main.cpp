60bb8d44d21849adb7d75f54272a9ab3b8933bcd
Logan Rose <logprose@umich.edu>
2026-05-30 11:05:27 -0400

    feat: scaffolding for benchmarking

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,23 @@
 #include <print>
 #include "lamport_queue.h"
+#include "optimized_ring_buffer.h"
+#include "mutex_queue.h"
 #include <thread>
 
 
 const int ITERATIONS = 1000;
 
-void producer(SPSC::LamportQueue<int, ITERATIONS>& q);
-void consumer(SPSC::LamportQueue<int, ITERATIONS>& q);
+void ringbuf_producer(SPSC::RingBuffer<int, ITERATIONS>& q);
+void ringbuf_consumer(SPSC::RingBuffer<int, ITERATIONS>& q);
+void lamport_producer(Lamport::LamportQueue<int, ITERATIONS>& q);
+void lamport_consumer(Lamport::LamportQueue<int, ITERATIONS>& q);
+void mutex_producer(Mutex::MutexQueue<int, ITERATIONS>& q);
+void mutex_consumer(Mutex::MutexQueue<int, ITERATIONS>& q);
 
 int main() {
-    SPSC::LamportQueue<int, ITERATIONS> queue;
-    std::thread p{producer, std::ref(queue)};
-    std::thread c{consumer, std::ref(queue)};
+    SPSC::RingBuffer<int, ITERATIONS> queue;
+    std::thread p{ringbuf_producer, std::ref(queue)};
+    std::thread c{ringbuf_consumer, std::ref(queue)};
     p.join();
     c.join();
 
@@ -19,13 +25,41 @@ int main() {
 }
 
 
-void producer(SPSC::LamportQueue<int, ITERATIONS>& q) {
+void ringbuf_producer(SPSC::RingBuffer<int, ITERATIONS>& q) {
     for (int i = 0; i < ITERATIONS; i++) {
         q.push(i);
     }
 }
 
-void consumer(SPSC::LamportQueue<int, ITERATIONS>& q) {
+void ringbuf_consumer(SPSC::RingBuffer<int, ITERATIONS>& q) {
+    for (int i = 0; i < ITERATIONS; i++) {
+        if (auto val = q.pop()) {
+            std::print("consumer value: {}\n", *val);
+        }
+    }
+}
+
+void lamport_producer(Lamport::LamportQueue<int, ITERATIONS>& q) {
+    for (int i = 0; i < ITERATIONS; i++) {
+        q.push(i);
+    }
+}
+
+void lamport_consumer(Lamport::LamportQueue<int, ITERATIONS>& q) {
+    for (int i = 0; i < ITERATIONS; i++) {
+        if (auto val = q.pop()) {
+            std::print("consumer value: {}\n", *val);
+        }
+    }
+}
+
+void mutex_producer(Mutex::MutexQueue<int, ITERATIONS>& q) {
+    for (int i = 0; i < ITERATIONS; i++) {
+        q.push(i);
+    }
+}
+
+void mutex_consumer(Mutex::MutexQueue<int, ITERATIONS>& q) {
     for (int i = 0; i < ITERATIONS; i++) {
         if (auto val = q.pop()) {
             std::print("consumer value: {}\n", *val);
