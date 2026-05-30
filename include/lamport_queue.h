d22933e5aadc58d7c1d2b801e0f382b612ba4f45
Logan Rose <logprose@umich.edu>
2026-05-30 09:52:49 -0400

    chore: tsan still reporting data race with std::memory_order_relaxed

--- include/lamport_queue.h
+++ include/lamport_queue.h
@@ -19,8 +19,8 @@ namespace SPSC {
     bool LamportQueue<T, N>::push(T elem) {
         std::size_t back, front;
 
-        back = std::atomic_load_explicit(&back_, std::memory_order_seq_cst);
-        front = std::atomic_load_explicit(&front_, std::memory_order_seq_cst);
+        back = std::atomic_load_explicit(&back_, std::memory_order_relaxed);
+        front = std::atomic_load_explicit(&front_, std::memory_order_relaxed);
         if ((back + 1) % N == front) {
             return false;
         } else {
@@ -29,22 +29,22 @@ namespace SPSC {
 
         data_[back] = elem;
         // give back_, store the size remaining, enforce memory sequencing
-        std::atomic_store_explicit(&back_, (back + 1) % N, std::memory_order_seq_cst);
+        std::atomic_store_explicit(&back_, (back + 1) % N, std::memory_order_relaxed);
         return true;
     }
 
     template <typename T, std::size_t N>
     std::optional<T> LamportQueue<T, N>::pop() {
         std::size_t back, front;
-        front = std::atomic_load_explicit(&front_, std::memory_order_seq_cst);
-        back = std::atomic_load_explicit(&back_, std::memory_order_seq_cst);
+        front = std::atomic_load_explicit(&front_, std::memory_order_relaxed);
+        back = std::atomic_load_explicit(&back_, std::memory_order_relaxed);
         if (back == front) {
             return std::nullopt;
         } else {
 
         }
         T elem = data_[front];
-        std::atomic_store_explicit(&front_, (front + 1) % N, std::memory_order_seq_cst);
+        std::atomic_store_explicit(&front_, (front + 1) % N, std::memory_order_relaxed);
         return elem;
     }
 
